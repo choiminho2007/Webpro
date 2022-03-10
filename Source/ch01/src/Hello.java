ada1ab4571eda7e84dabb528ddeef050c8fb6a06
choiminho2007 <choiminho2007@naver.com>
2022-03-10 17:00:38 +0900

    first

---
+++ Source/ch01/src/Hello.java
@@ -0,0 +1,22 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, Java");
+		System.out.println("Welcome~");
+		
+		// 한줄 주석 - 설명문(컴퓨터는 실행하지 않는 실행문)
+		/*
+		 2022.3.10 첫번째 자바 프로그램. 홍길동
+		 */
+		
+		System.out.print("Welcome~");
+		System.out.println("끝");
+		
+		// 콘솔 출력 : println(개행포함), print(개행미포함)
+		
+		
+	}
+
+}
